99575ccfbffa3d54308f9807227c683cf33ebfbd
Marco Moya <zgmf_x235@hotmail.com>
2025-02-11 16:39:17 -0600

    cambio de nombre en la carpeta de compilación de TS

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"use strict";
+function calculateAge(birthdate) {
+    const today = new Date();
+    const birth = new Date(birthdate + "T00:00:00");
+    let age = today.getFullYear() - birth.getFullYear();
+    const currentMonth = today.getMonth() + 1;
+    const monthOfBirth = birth.getMonth() + 1;
+    if (currentMonth < monthOfBirth || (currentMonth === monthOfBirth && today.getDate() < birth.getDate())) {
+        age--;
+    }
+    return age;
+}
+// Ejemplo de uso
+const birthdate = '1990-05-15';
+const age = calculateAge(birthdate);
+console.log(`La edad es: ${age}`);
